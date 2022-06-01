c125621fc9c5f095d54f5c513a4c6a7a4b0670b0
FURY <79848348+FURY312@users.noreply.github.com>
2022-06-01 22:01:40 +0900

    V2.1

--- IGB-v2.0.py
+++ IGB-v2.0.py
@@ -28,41 +28,45 @@ def bot():
     driver = webdriver.Chrome(driver_path, options=options)
     driver.implicitly_wait(10)
 
-    #인스타그램 페이지로 이동
     driver.get('https://www.instagram.com/?hl=ko')
     print('로그인중....')
     time.sleep(5)
 
-    #아이디 입력
+    #아이디
 
     id_input = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label')
     id_input.click()
-    id_input.send_keys(id) #아이디 입력
+    id_input.send_keys(id)
 
-    #패스워드 입력
+    #PW
 
     pw_input = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label')
     pw_input.click()
     pw_input.send_keys(pw)
     time.sleep(1)
 
-    #로그인 버튼 클릭
+    #로그인 버튼
 
     login_btn = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
     login_btn.click()
 
-    time.sleep(6)
+    time.sleep(4)
+
+    #확인버튼
+    oa_btn = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/section/main/div/div/div/section/div/button')
+    oa_btn.click()
+    time.sleep(3)
 
     #이동
     driver.get('https://www.instagram.com/explore/tags/{}/'.format(tag))
     time.sleep(5)
 
-    #게시물 첫번째 피드 선택
+    #최근피드 선택
     if ff==1:
         first_feed = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/section/main/article/div[2]/div/div[1]/div[1]/a/div/div[2]')
         first_feed.click()
         time.sleep(2)
-    #게시물 인기 피드 선택
+    #인기피드 선택
     if ff==0:
         first_feed = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/section/main/article/div[1]/div/div/div[1]/div[1]/a/div[1]')
         first_feed.click()
@@ -77,7 +81,7 @@ def bot():
         el_list2 = driver.find_elements_by_xpath(xpath2)
         time.sleep(1)
         #다음
-        nextFeed = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[2]')
+        nextFeed = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div')
         nextFeed.click()
         time.sleep(2+random.random()) 
     print('===========모든 작업 완료==============')
